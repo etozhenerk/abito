f33ae956ae3553e29b8400c1facff614c8fb9611
etozhenerk <etozhenerk@yandex.ru>
2021-07-30 00:00:04 +0300

    media index

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
-'use strict';
+"use strict";
 
-var swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper(".mySwiper", {
   spaceBetween: 10,
   slidesPerView: 6,
   freeMode: true,
   watchSlidesVisibility: true,
   watchSlidesProgress: true,
 });
-var swiper2 = new Swiper(".mySwiper2", {
+const swiper2 = new Swiper(".mySwiper2", {
   spaceBetween: 10,
   thumbs: {
     swiper: swiper,
   },
+});
+
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".navbar-panel");
+const menuClose = document.querySelector(".menu-close");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle('is-open');
+});
+menuClose.addEventListener("click", () => {
+  menu.classList.toggle('is-open');
 });
